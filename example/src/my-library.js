f2f543ae267fc7c01f5e807b9ebc6ac876ce2836
Danny Kim <0916dhkim@gmail.com>
2025-07-25 23:57:01 -0400

    Cleanup

--- example/src/my-library.js
+++ example/src/my-library.js
@@ -1,5 +1,5 @@
 // Mock CSS-in-JS library for testing
 export function css(styles) {
   // This is just a mock - the plugin will replace these calls
-  return JSON.stringify(styles);
+  return "css-mock";
 }
